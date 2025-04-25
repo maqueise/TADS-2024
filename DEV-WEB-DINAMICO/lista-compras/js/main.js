35d06dabb758f4f7020884b469676cc40c772495
Caio Maqueise Alécio Pinheiro <caio.pinheiro@ifms.edu.br>
2025-04-24 22:37:49 -0300

    aula 24-04 finalizando exercicios array

--- DEV-WEB-DINAMICO/lista-compras/js/main.js
+++ DEV-WEB-DINAMICO/lista-compras/js/main.js
@@ -1,22 +1,50 @@
 let inputItem = document.querySelector('#item')
 let btnAdicionar = document.querySelector('#btnAdicionar')
+let btnAdicionarInicio = document.querySelector('#btnAdicionarInicio')
+let btnRemoverUltimo = document.querySelector('#btnRemoverUltimo')
+let btnRemoverInicio = document.querySelector('#btnRemoverInicio')
 let ulListaCompras = document.querySelector('#listaCompras')
 let divTotalItens = document.querySelector('#total')
 
 let listaCompras = []
+
 if(localStorage.getItem('lista')){
     listaCompras = JSON.parse(localStorage.getItem('lista'))
 }
 
 function adicionarItem(){
    let item = inputItem.value
+   if(!validarDados(item)) return
    listaCompras.push(item)
    renderLista()
    atualizarTotal()
-   localStorage.setItem('lista',JSON.stringify(listaCompras))
+   salvarDados()
+}
+function adicionarItemInicio(){
+    let item = inputItem.value
+    if(!validarDados(item)) return
+    listaCompras.unshift(item)
+    renderLista()
+    atualizarTotal()
+    salvarDados()
+}
+function removerUltimo(){
+    listaCompras.pop()
+    renderLista()
+    atualizarTotal()
+    salvarDados()
+}
+function removerInicio(){
+    listaCompras.shift()
+    renderLista()
+    atualizarTotal()
+    salvarDados()
 }
 function atualizarTotal(){
     divTotalItens.textContent = listaCompras.length
+}
+function salvarDados(){
+    localStorage.setItem('lista',JSON.stringify(listaCompras))
 }
 function renderLista(){
     ulListaCompras.innerHTML = ''
@@ -27,7 +55,18 @@ function renderLista(){
         ulListaCompras.appendChild(li)
     }
 }
+function validarDados(item){
+    if(item.trim() ==''){
+        alert('insira uma descrição')
+        return false
+    }else{
+        return true
+    }
+}
 renderLista()
 atualizarTotal()
 
 btnAdicionar.onclick = adicionarItem
+btnAdicionarInicio.onclick = adicionarItemInicio
+btnRemoverUltimo.onclick = removerUltimo
+btnRemoverInicio.onclick = removerInicio
